1138f253d49a5fb4a5651e0a50beae51a171e22c
Rijvy <rrijvy@gmail.com>
2018-10-11 04:26:50 +0600

    Add project files.

---
+++ OnlineAlbum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineAlbum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
